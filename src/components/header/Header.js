8eb6e44eb20d1d4cd39dae50871d3b282e4f7b29
Neel Sheth <nsheth109@gmail.com>
2021-06-07 18:52:03 -0400

    created header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ function Header () {
     return (
         <div className = "header py-5">
             <h1>Employee Directory</h1>
-            <p>Click on the <span><i class="bi bi-sort-alpha-down"></i></span> or the <span><i class="bi bi-sort-alpha-up-alt"></i></span> to sort the employees names</p>
+            <p>Click on the sort buttons to filter the employees names or use the search bar to narrow results. </p>
         </div>
 
     );
