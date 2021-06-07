317e40638f6c58ad19ab8f183982acd2632dd0e5
Neel Sheth <nsheth109@gmail.com>
2021-06-07 18:55:06 -0400

    added body component, sort utility, and updated app.js with the componentDidMount to get the api to return data. Also updated package.json with more dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import API from "../src/utils/API"
+import API from "./utils/API"
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
+import Results from "./components/results/Results";
+import Moment from 'moment';
+import  {sortItems}  from "./utils/sort";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  class App extends Component {
+    state = {
+      search: "",
+      results: [],
+      filteredresults:[],
+      sortAsc:true,
+      error: ""
+    };
+  
+    componentDidMount() {
+      API.getData()
+        .then(res => {
+          var resultItems= res.data.results.map(data=>{return {Image:data.picture.medium, Name:data.name.first+' '+data.name.last,Phone:data.phone,Email:data.email, DOB:Moment(data.dob.date).format('MM-DD-YYYY')  }});
+          this.setState({ results:resultItems,filteredresults:sortItems(resultItems,this.state.sortAsc) });
+        }
+        )
+        .catch(err => console.log(err));
+    }
+    handleInputChange = event => {
+     
+      this.setState({filteredresults: sortItems( this.state.results
+        .filter(item => {
+          let values = Object.values(item).join("").toLowerCase()
+          return values.indexOf(event.target.value.toLowerCase()) !== -1;
+      }),this.state.sortAsc)
+      });
+     
+    };
+  
+    handleSortChange = event => {
+      var sortAsc=!this.state.sortAsc;
+      this.setState({sortAsc: sortAsc});
+      this.setState({filteredresults:sortItems( this.state.filteredresults,sortAsc)});
+     };
+   
+  
+    render() {
+    return (
+      <div>
+      <Header />
+      <Body  
+              handleInputChange={this.handleInputChange}
+             />
+               <Results handleSortChange={this.handleSortChange} results={this.state.filteredresults} sortAsc={this.state.sortAsc} />
+      </div>
+    );
+  
+  }
+  }
+  
+  export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ function Header () {
     return (
         <div className = "header py-5">
             <h1>Employee Directory</h1>
-            <p>Click on the sort buttons to filter the employees names or use the search bar to narrow results. </p>
+            <p>Click on the sort button to filter the employees names or use the search bar to narrow results. </p>
         </div>
 
     );
